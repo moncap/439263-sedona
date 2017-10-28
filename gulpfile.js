36b3d061ea9ad947ee6a174a1fedc0d89fda7eb0
MaLkoLmX <kadirovfamily@rambler.ru>
2017-10-28 22:28:47 +0400

    Проверка итогового проекта «Седона»

--- gulpfile.js
+++ gulpfile.js
@@ -6,72 +6,21 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
-var rename = require("gulp-rename");
-var svgstore = require("gulp-svgstore");
-var imagemin = require("gulp-imagemin");
-var minify = require("gulp-csso");
-var webp = require("gulp-webp");
-var del = require("del");
-var run = require("run-sequence");
 
-gulp.task("copy", function () {
-  return gulp.src([
-      "fonts/**/*.{woff,woff2}",
-      "img/**",
-      "js/**",
-      "*.html"
-    ], {
-      base: "."
-    })
-    .pipe(gulp.dest("build"));
-});
-
-gulp.task("clean", function () {
-  return del("build");
-});
-
-gulp.task("style", function () {
+gulp.task("style", function() {
   gulp.src("less/style.less")
     .pipe(plumber())
     .pipe(less())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("build/css"))
-    .pipe(minify())
-    .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("build/css"));
+    .pipe(gulp.dest("css"))
+    .pipe(server.stream());
 });
 
-gulp.task("images", function () {
-  return gulp.src("img/**/*.{png,jpg,svg}")
-    .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true}),
-      imagemin.svgo()
-    ]))
-    .pipe(gulp.dest("build/img"));
-});
-
-gulp.task("sprite", function () {
-  return gulp.src("img/icon-*.svg")
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
-    .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("build/img"));
-});
-
-gulp.task("webp", function () {
-  return gulp.src("img/**/*.{png,jpg}")
-    .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("build/img"));
-});
-
-
-gulp.task("serve", function() {
+gulp.task("serve", ["style"], function() {
   server.init({
-    server: "build/",
+    server: ".",
     notify: false,
     open: true,
     cors: true,
@@ -80,8 +29,4 @@ gulp.task("serve", function() {
 
   gulp.watch("less/**/*.less", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
-});
-
-gulp.task("build", function (done) {
-  run("clean", "copy", "style", "images", "sprite", "webp", done);
 });
